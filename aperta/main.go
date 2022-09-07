932085c49f7e7b247a7aa190a5c8d49734b1990c
chetansha256 <chetan.2008.sharma@gmail.com>
2022-09-07 22:16:16 +0200

    Aperta service: this commit includes * Moving all files from auth to open folder. * Adding Dockerfile to build. * Some lint suggested updates

--- aperta/main.go
+++ aperta/main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/go-kit/log"
 	"net/http"
 	"os"
-	"swednabler/aperta/v2/auth"
+	"swednabler/aperta/v2/open"
 )
 
 // The main method
@@ -13,7 +13,7 @@ func main() {
 	logger = log.NewLogfmtLogger(os.Stderr)
 	logger = log.With(logger, "ts", log.DefaultTimestampUTC, "listen", "8081", "caller", log.DefaultCaller)
 	// the http server that listens on 8081 and pass the incoming request to the router
-	r := auth.NewHttpServer(auth.NewService(), auth.NewJwtService(), logger)
+	r := open.NewHttpServer(open.NewService(), open.NewJwtService(), logger)
 	_ = logger.Log("msg", "HTTP", "addr", "8081")
 	_ = logger.Log("err", http.ListenAndServe(":8081", r))
 }
